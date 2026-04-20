22cc87c0f09d13279be5dbf4bd1bfa76c4a49ae3
jjaytm <jjaytnn@gmail.com>
2026-04-19 21:58:49 -0400

    Add S3 remote backend for Terraform state sharing with GitHub Actions

--- infra/main.tf
+++ infra/main.tf
@@ -3,6 +3,13 @@ terraform {
   required_providers {
     aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
+
+  backend "s3" {
+    bucket         = "job-coach-tfstate-490841876944"
+    key            = "career-coach/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
